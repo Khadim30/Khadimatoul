f950bdf4fd5764fcb0220658a7d229c2374245f8
ahmadoubamba <ahmadoubambandour54@gmail.com>
2026-06-21 14:24:17 +0000

    fix navbar import for vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import Navbar from "./components/Navbar";
+// import Navbar from "./components/Navbar";
+import Navbar from "./components/Navbar.jsx";
 import AppRoutes from "./routes/AppRoutes";
 
 function App() {
